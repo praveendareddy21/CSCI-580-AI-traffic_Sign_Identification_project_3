bd86f291ce8a0a6a0ec0ac2253fdb50e0855b948
matthewpdias <matthewpdias>
2016-10-22 23:33:18 -0700

    fixed

--- assignment1/robot.h
+++ assignment1/robot.h
@@ -4,7 +4,7 @@
 #include <string>
 
 class robot{
-   long double bitDiff[5]={0.6561, 0.0729, 0.0081, 0.0009, 0.00001};
+   long double bitDiff[5]={0.6561, 0.0729, 0.0081, 0.0009, 0.0001};
 public:
   int numObservations;
   string *observations;
